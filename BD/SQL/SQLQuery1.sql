a198513cbd981eec285ef10d60cfb7279495e800
Student <student@abv.bg>
2024-10-11 11:54:21 +0300

    add

--- BD/SQL/SQLQuery1.sql
+++ BD/SQL/SQLQuery1.sql
@@ -18,7 +18,7 @@ CREATE TABLE Cars
   Description  NVARCHAR(256),
   Price DECIMAL(8,2) CONSTRAINT Valid_Price CHECK (Price>=0) NOT NULL,
   HorsePower INT NOT NULL,
-  EngineType NVARCHAR,
+  EngineType NVARCHAR(128),
   CarManufacturerId INT CONSTRAINT FK_Cars_CarManufacturers
   FOREIGN KEY REFERENCES  CarManufacturers(Id) NOT NULL
 
@@ -26,6 +26,23 @@ CREATE TABLE Cars
 GO
 
 INSERT INTO CarManufacturers(Id, Name, Email,Adress) VALUES
+(1,'Audi','audiofficial@gmail.com','Berlin  9 September street'),
+(2,'BMW','BMWofficial@abv.ge','Frankfurt Reinriver street'),
+(3,'Mercedes','mercedesbenz@yahoo.com',' Sofia Georgi Rakovski street'),
+(4,'Dachia','dachiaofficial.abv.re','Bucurest Strada Maria street')
+
+GO
+INSERT INTO Cars(CarManufacturerId,Model, Description,Price,HorsePower,EngineType) VALUES
+(1,'Audi RSQ8','2022 model super moshten i iconomichen model',185000.00,'560','V8'),
+(3,'Mercedes CLS','2023 model moshten,semeen avtomobil',145000.00,'457','3,0 – 6,2 benzin'),
+(4,'Dachia Duster','2020 model super semeen iconomichen model',56000.00,'225','V6'),
+(2,'BMW M5','2022 model super moshten,burz i krasiv',250000.00,'650','V8')
+
+SELECT* FROM CarManufacturers
+
+SELECT* FROM Cars
+
+DROP TABLE Cars
 
 
 
---
+++ BD/tablici i klasove/tablici i klasove/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace tablici_i_klasove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class Car
+        {
+
+        }
+
+        public class CarManufacturer
+        {
+            public virtual List<Car> Cars { get; set; };
+        }
+
+
+    }
+}
