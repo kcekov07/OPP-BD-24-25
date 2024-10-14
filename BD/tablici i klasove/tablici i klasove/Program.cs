c6e1464da405034b7bc116369e02a18dc59ba7a9
Student <student@abv.bg>
2024-10-14 11:07:07 +0300

    add

--- BD/tablici i klasove/tablici i klasove/Program.cs
+++ BD/tablici i klasove/tablici i klasove/Program.cs
@@ -8,12 +8,25 @@
         }
         public class Car
         {
+            public int CarManufacturerId { get; set; } 
 
+            public virtual CarManufacturer CarManufactuer { get; set; }
+            public int Id { get; set; } 
+            public int Model { get; set; } 
+            public int Description { get; set; } 
+            public int Price { get; set; } 
+            public int HorsePower { get; set; } 
+            public int EngineType { get; set; } 
         }
 
         public class CarManufacturer
         {
-            public virtual List<Car> Cars { get; set; };
+            public virtual List<Car> Cars { get; set; }
+            public int Id { get; set; }
+            public int Name { get; set; }
+            public int Email{ get; set; }
+            public int Adress { get; set; }
+
         }
 
 
